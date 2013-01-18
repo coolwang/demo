285bf46d6e7612b163d9a33a622026195522393c
wecan <wecan@weknowwecan.com>
2013-01-17 22:58:03 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '0fa7597f6f071bacf4710f9dfc65fda981701bfbea28bcedb33ed25620c05a2884a4c80ab88adf74e096583b6264b2fafc72dd647355f5bc25004574328b2297'
